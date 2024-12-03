968e08c30904c6b379faf10520009dd31cba01e5
G.l.s.t.n.d <glistandirovka@gmail.com>
2024-12-03 23:59:22 +0300

    изменены cors

--- Backend/services/accounts/app/web/cors.py
+++ Backend/services/accounts/app/web/cors.py
@@ -3,13 +3,15 @@ from aiohttp.web_app import Application
 
 
 def setup_cors(app: Application):
+    # Configure default CORS settings.
     cors = aiohttp_cors.setup(app, defaults={
         "*": aiohttp_cors.ResourceOptions(
             allow_credentials=True,
             expose_headers="*",
-            allow_headers="*"
+            allow_headers="*",
         )
     })
 
+    # Configure CORS on all routes.
     for route in list(app.router.routes()):
         cors.add(route)
