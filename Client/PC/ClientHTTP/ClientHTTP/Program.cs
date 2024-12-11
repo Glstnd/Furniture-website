8cc5b204348f3432c2b4569656150dd04022c8e2
G.l.s.t.n.d <glistandirovka@gmail.com>
2024-12-11 15:40:06 +0300

    Добавлен самый простой клиент на C# 9

---
+++ Client/PC/ClientHTTP/ClientHTTP/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ClientHTTP;
+
+internal static class Program
+{
+    static async Task Main()
+    {
+        string apiUrl = "http://185.244.219.179:8080/api/catalogs/list";
+        
+        using (HttpClient client = new HttpClient())
+        {
+            HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
+        }
+    }
+}
